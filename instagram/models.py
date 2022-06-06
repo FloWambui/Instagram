55ad7a362629c5dcdca3cc14383f7836c7091933
FloWambui <gflorencewambui@gmail.com>
2022-06-06 15:09:58 +0300

    add the profile models

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField 
+import datetime as dt
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -10,6 +12,8 @@ class Image(models.Model):
     image_date = models.DateTimeField(auto_now_add=True)
     like_count = models.IntegerField(default=0)
     comment_count = models.IntegerField(default=0)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def save_image(self):
         self.save()
@@ -35,3 +39,26 @@ class Image(models.Model):
     def __str__(self):
         return self.image_name
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    contact = models.CharField(max_length=50, blank=True, null=True)
+
+    def update(self):
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_by_user(cls, user):
+        profile = cls.objects.filter(user=user)
+        return profile
+
+    def __str__(self):
+        return self.user.username
